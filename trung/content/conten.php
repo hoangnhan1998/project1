7a23bcb437e25285d77d6d28907009f77c84b937
VanBinh <truong0908334716@gmail.com>
2019-06-10 12:07:40 +0700

    sua

--- trung/content/conten.php
+++
@@ -1,6 +0,0 @@
-<div class="container" style="height:auto; width:1350px">
-	<div class="sidenav" style="float:left; margin-right:20px; width:200px">
-		<?php include('content/content.php');?>
-    </div>
-		<?php include('content/content2.php') ?>
-</div>
--- trung/content/content.php
+++
@@ -1,4 +0,0 @@
-<div class="container" style="height:500px; width:1350px" >
-            	
-		</div> 	
-        
