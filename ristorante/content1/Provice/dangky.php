dab810c5ef33387537f0989fb208226e8c9f03f7
VanBinh <truong0908334716@gmail.com>
2019-06-13 21:22:39 +0700

    hi

--- ristorante/content1/Provice/dangky.php
+++ ristorante/content1/Provice/dangky.php
@@ -1,70 +1,58 @@
 <?php
-
-	$_SERVER['host'] = 'localhost';
-	$_SERVER['users'] = 'root';
-	$_SERVER['passwd'] = '';
-	$_SERVER['database'] = 'webphukiendt';
-
-	$query = mysql_connect($_SERVER['host'],$_SERVER['users'],$_SERVER['passwd']);
-	mysql_select_db($_SERVER['database']);
-	mysql_query("SET NAMES 'UTF8'");
-
-
-	if(isset($_POST['gui'])){
-		$tenkh=$_POST['hoten'];
-		$email=$_POST['email'];
-		$diachi=$_POST['diachi'];
-		$pass=$_POST['pass'];
-		$dienthoai=$_POST['dienthoai'];
-		$sql_dangky=mysql_query("insert into dangky (tenkhachhang,email,matkhau,dienthoai,diachinhan) value('$tenkh','$email','$pass','$dienthoai','$diachi')");
-		    
-	if($sql_dangky){
-		echo '<h3 style="margin-left:5px;">Bạn đã đăng ký thành công</h3>';
-		echo '<a href="?quanly=dangnhap" style="margin:20px;text-decoration:none;">Quay lại để đăng nhập mua hàng</a>';
-	}
-	}
+  if(isset($_POST['gui'])){
+    $tenkh=$_POST['hoten'];
+    $email=$_POST['email'];
+    $diachi=$_POST['diachi'];
+    $pass=$_POST['pass'];
+    $dienthoai=$_POST['dienthoai'];
+    $sql="select email from dangky where email='$email'";
+    $ki=mysql_query($sql);
+    $row=mysql_num_rows($ki);
+    if($row!=$email){
+      echo"<script>alert('Tài Khoản này đã tồn tại')</script>";
+      }else{
+    $sql_dangky=mysql_query("insert into dangky (tenkhachhang,email,matkhau,dienthoai,diachinhan) value('$tenkh','$email','$pass','$dienthoai','$diachi')");
+        
+        if($sql_dangky){
+    echo '<h3 style="margin-left:5px;">Bạn đã đăng ký thành công</h3>';
+    echo '<a href="?quanly=login" style="margin:20px;text-decoration:none;">Quay lại để đăng nhập mua hàng</a>';
+  }}
+  }
 ?>
-	
 
 <div>
-	HOAN NGHÊNH QUÝ BẠN ĐẶT HÀNG TẠI XƯỞNG PHỤ KIỆN
-</div>
-<div>
-  <p style="font-size:18px; color:red;margin:5px;">Các mục dấu * là bắt buộc tối thiểu. Vui lòng điền đầy đủ và chính xác (Số nhà, Ngõ, thôn xóm/ấp, Phường/xã, huyện/quận, tỉnh, TP)</p>
-  <form method="post" enctype="multipart/form-data">
-	<table width="100%" border="1" style="border-collapse:collapse;">
-  <tr>
-    <td width="40%">Họ tên người mua <strong style="color:red;"> (*)</strong></td>
-    <td width="60%"><input type="text" name="hoten" size="50"></td>
-  </tr>
-  <tr>
-    <td>Địa chỉ Email <strong style="color:red;"> (*)</strong></td>
-    <td width="60%"><input type="text" name="email" size="50"></td>
-  </tr>
-  <tr>
-    <td>Mật khẩu  <strong style="color:red;"> (*)</strong></td>
-    <td width="60%"><input type="password" name="pass" size="50"></td>
-  </tr>
-  <tr>
-    <td>Điện thoại <strong style="color:red;"> (*)</strong></td>
-     <td width="60%"><input type="text" name="dienthoai" size="50"></td>
-  </tr>
-  <tr>
-    <td>Địa chỉ nhận hàng <strong style="color:red;"> (*)</strong></td>
-   <td width="60%"><input type="text" name="diachi" size="50"></td>
-  </tr>
-  <tr>
-    <td colspan="2">
-    	 	
-           <p><input type="submit" name="gui" value="Gửi thông tin" /></p>
-         
-    </td>
+<p style="font-size:18px; color:red;margin:5px;">Các mục dấu * là bắt buộc tối thiểu. Vui lòng điền đầy đủ và chính xác (Số nhà, Ngõ, thôn xóm/ấp, Phường/xã, huyện/quận, tỉnh, TP)</p>
+<form method="post" enctype="multipart/form-data">
+  <table width="100%" border="1" style="border-collapse:collapse;">
+    <tr>
+      <td width="40%">Họ tên người mua <strong style="color:red;"> (*)</strong></td>
+      <td width="60%"><input type="text" name="hoten" size="50"></td>
     </tr>
-</table>
+    <tr>
+      <td>Địa chỉ Email <strong style="color:red;"> (*)</strong></td>
+      <td width="60%"><input type="text" name="email" size="50"></td>
+    </tr>
+    <tr>
+      <td>Mật khẩu <strong style="color:red;"> (*)</strong></td>
+      <td width="60%"><input type="password" name="pass" size="50"></td>
+    </tr>
+    <tr>
+      <td>Điện thoại <strong style="color:red;"> (*)</strong></td>
+      <td width="60%"><input type="text" name="dienthoai" size="50"></td>
+    </tr>
+    <tr>
+      <td>Địa chỉ nhận hàng <strong style="color:red;"> (*)</strong></td>
+      <td width="60%"><input type="text" name="diachi" size="50"></td>
+    </tr>
+    <tr>
+      <td colspan="2"><p>
+          <input type="submit" name="gui" value="Gửi thông tin" />
+        </p></td>
+    </tr>
+  </table>
 </form>
 <div>
-	<p>Ghi chú nếu có :</p>
-	<textarea name="ghichu">
+<p>Ghi chú nếu có :</p>
+<textarea name="ghichu">
     
     </textarea>
-
--- ristorante/content1/menu.php
+++ ristorante/content1/menu.php
@@ -7,16 +7,16 @@
         </form></div>
     <div class="navbar-collapse" id="navbarText">
       <ul class="navbar-nav">
-      	<li class="nav-item" style="float:left"><a class="nav-link" href="#home"></a></li> 
+        <li class="nav-item" style="float:left"><a class="nav-link" href="#home"></a></li> 
         <li class="nav-item"> <a class="nav-link" href="#home">Home</a> </li>
         <li class="nav-item"> <a class="nav-link" href="?quanly=lichsu&&#Restaurant" >Lịch sử </a> </li>
         <li class="nav-item"> <a class="nav-link" href="?quanly=chitietsp&&#Restaurant" >Sản Phẩm</a> </li>
         <li class="nav-item"> <a class="nav-link" href="?quanly=dathang&&#Restaurant" >Giỏ Hàng</a> </li>
         <?php if(isset($_SESSION['email'])) { 
         echo"<li class='nav-item'> <a class='nav-link' href='?quanly=logout'>Đăng Xuất</a> </li>
-			<li class='nav-item'> <a class='nav-link' href='?quanly=sua&&#Restaurant' >Provie</a> </li>
-			";}
-        else echo"<li class='nav-item'> <a class='nav-link' href='?quanly=login#Restaurant'>Đăng Nhập</a> </li>";?>
+      <li class='nav-item'> <a class='nav-link' href='?quanly=sua&&#Restaurant' >Provie</a> </li>
+      ";}
+        else echo"<li class='nav-item'> <a class='nav-link' href='?quanly=login#Restaurant'>Đăng Nhập</a> </li><li class='nav-item'> <a class='nav-link' href='?quanly=dangky#Restaurant'>Đăng Ký</a> </li>";?>
         <li class="nav-item"> <a class="nav-link" href="#OurLocation" >Our Location</a> </li>
       </ul>
     </div>
